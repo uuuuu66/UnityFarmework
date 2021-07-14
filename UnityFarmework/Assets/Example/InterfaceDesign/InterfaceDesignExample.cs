b3d11b2d7790c239c5f1ca863b70c5af1fc35caa
xiaojun123 <359945657@qq.com>
2021-07-15 01:15:05 +0800

    接口显示调用

---
+++ UnityFarmework/Assets/Example/InterfaceDesign/InterfaceDesignExample.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICanSayHello
+{
+    void SayHello();
+    void SayOther();
+}
+
+public class InterfaceDesignExample : MonoBehaviour,ICanSayHello
+{
+    public void SayHello()
+    {
+        Debug.Log("Hello");
+    }
+
+    void ICanSayHello.SayOther()
+    {
+        Debug.Log("Other");
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.SayHello();
+        (this as ICanSayHello).SayOther();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityFarmework/Assets/Example/InterfaceDesign/InterfaceStructExample.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+
+
+public class InterfaceStructExample : MonoBehaviour
+{
+    public interface ICustomScript
+    {
+        void Start();
+        void Update();
+        void Destroy();
+    }
+
+
+    public abstract class CustomSctipt : ICustomScript
+    {
+        //防止循环调用，所以借口显示调用
+        void ICustomScript.Destroy()
+        {
+            OnDestroy();
+        }
+
+        void ICustomScript.Start()
+        {
+            OnStart();
+        }
+
+        void ICustomScript.Update()
+        {
+            OnUpdate();
+        }
+
+        protected abstract void OnStart();
+        protected abstract void OnUpdate();
+        protected abstract void OnDestroy();
+    }
+
+    public class MyScript : CustomSctipt
+    {
+        protected override void OnDestroy()
+        {
+            Debug.Log("On Start");
+        }
+
+        protected override void OnStart()
+        {
+            Debug.Log("On Update");
+        }
+
+        protected override void OnUpdate()
+        {
+            Debug.Log("On Destroy");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ICustomScript myScript = new MyScript();
+        myScript.Start();
+        myScript.Update();
+        myScript.Destroy();
+    }
+
+}
